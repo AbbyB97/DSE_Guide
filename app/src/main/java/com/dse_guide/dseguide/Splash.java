f816a5361cc46a4f57f4f026f09bed6cd93b9b10
AbbyB97 <bansodeabhijit1997@gmail.com>
2016-07-28 21:41:17 +0530

    splash screen developed

--- app/src/main/java/com/dse_guide/dseguide/Splash.java
+++ app/src/main/java/com/dse_guide/dseguide/Splash.java
@@ -3,16 +3,22 @@ package com.dse_guide.dseguide;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.WindowManager;
+import android.widget.ImageView;
 
 import com.dse_guide.dseguide.DSE_guide.DSE_guide_dashboard;
+import com.flaviofaria.kenburnsview.KenBurnsView;
 
 public class Splash extends AppCompatActivity {
-
+    private KenBurnsView kbv;
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.splash);
-
+        kbv=(KenBurnsView)findViewById(R.id.image);
+        kbv.setScaleType(ImageView.ScaleType.CENTER);
         //creating thread for splash screen
         Thread startTimer = new Thread() {
             public void run() {
